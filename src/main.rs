b39af3ab2b2310077c988f08f5db9cb664766dee
Shuyang Sun <shuyang@havenlife.com>
2020-10-23 00:56:36 -0400

    New demo code.

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,17 @@ fn main() {
     let res_1 = arr_1 + arr_2;
     // let _ = arr_2 + arr_1; // Error: thread 'main' panicked at 'ndarray: could not broadcast array from shape: [2, 2] to: [1, 2]'
     println!("arr_1 + arr_2  = {}", res_1);
+
+    let mut asdf = arr2(&[[1., 2.], [3., 4.]]);
+    asdf[[0, 0]] = 100.;
+    let mut asdf_dyn = asdf.into_dyn();
+    let view_1 = asdf_dyn.view();
+    println!("View 1: {}", view_1);
+    let back: ArrayView2<f64> = view_1.into_dimensionality().unwrap();
+    println!("{}", back);
+    let mut cde = asdf_dyn.view_mut();
+    cde.assign(&arr2(&[[2., 2.], [2., 2.]]));
+    println!("{}", asdf_dyn.view());
+    let fdddsfsf: ArrayView2<f64> = asdf_dyn.view().into_dimensionality().unwrap();
+    println!("{}", fdddsfsf);
 }
